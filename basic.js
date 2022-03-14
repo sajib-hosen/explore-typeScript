1e5895db8a0667ef36a80c9753738da7397d3645
sajib-hosen <sajib.201h@gmail.com>
2022-03-14 17:06:18 +0600

    this is first commit of typescript

---
+++ basic.js
@@ -0,0 +1,3 @@
+console.log('This is basic of TypeScript');
+var nuber1 = 10;
+console.log('the number is', nuber1);
---
+++ basic.tsx
@@ -0,0 +1,4 @@
+console.log('This is basic of TypeScript');
+
+const nuber1 = 10;
+console.log('the number is', nuber1 )
